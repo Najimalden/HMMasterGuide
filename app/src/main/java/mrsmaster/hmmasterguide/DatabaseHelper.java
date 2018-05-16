a290a934d92205ddcf1202ce4d6af386e25fc2cb
FK08 <du@du.de>
2018-05-16 14:47:16 +0200

    DatabaseHelper V1 is ready

--- app/src/main/java/mrsmaster/hmmasterguide/DatabaseHelper.java
+++ app/src/main/java/mrsmaster/hmmasterguide/DatabaseHelper.java
@@ -1,8 +1,18 @@
 package mrsmaster.hmmasterguide;
 
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
 /**
  * Created by FK08 on 16.05.2018.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteAssetHelper{
+    private static final String strDatabaseName = "MasterDB.db";
+    private static final int intDatabaseVersion = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context,strDatabaseName,null,intDatabaseVersion);
+    }
 }
