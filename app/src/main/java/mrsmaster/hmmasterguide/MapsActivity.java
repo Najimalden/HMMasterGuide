6f6fb97ba912f560eff7b8fc081341ba7cfb3e33
FK08 <ddd@dd.de>
2018-06-06 15:00:19 +0200

    google maps zoom funkts

--- app/src/main/java/mrsmaster/hmmasterguide/MapsActivity.java
+++ app/src/main/java/mrsmaster/hmmasterguide/MapsActivity.java
@@ -1,7 +1,11 @@
 package mrsmaster.hmmasterguide;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,7 +26,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
+
+
+
     }
+
 
 
     /**
@@ -39,9 +48,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(48.142758,11.568434);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Hochschule München (Karlstraße"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        LatLng hm = new LatLng(48.142758, 11.568434);
+        mMap.addMarker(new MarkerOptions().position(hm).title("Hochschule München (Karlstraße"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(hm, 15));
+
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+
+
+
+        mMap.setMyLocationEnabled(true);
+
+
     }
+
+
+
+
 }
